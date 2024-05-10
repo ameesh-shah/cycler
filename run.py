0b0b4913172520612b1345b31f4cba6470e7752b
ameesh-shah <ameesh@berkeley.edu>
2024-05-10 11:02:15 -0700

    fixing run

--- run.py
+++ run.py
@@ -21,15 +21,13 @@ def main(cfg):
     seeds = [np.random.randint(1e6) for _ in range(cfg.n_seeds)]
     np.random.seed(seeds[0])
     automaton = AutomatonRunner(Automaton(**cfg['ltl']))
-    # make logging dir for wandb to pull from, if necessary
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
     baseline = cfg["baseline"]
     if 'continuous' not in cfg['classes']:
         method = "q_learning"
     else:
         method = "ppo"
     for seed in seeds:
+        # make logging dir for wandb to pull from, if necessary
         save_dir = os.path.join(os.getcwd(), 'experiments', cfg['run_name'] + "_" + cfg["baseline"] + "_" + '_seed' + str(seed) + '_lambda' + str(cfg['lambda']))
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
